a89ee4b144e1faa4edbd2e95a2f99830cc6a16ec
Sanketh Mopuru <sanketh.mopuru@gmail.com>
2014-10-05 15:34:09 +0530

    Python packaging done

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-	name = "Coursera-Offline",
+	name = "Coursera Offline",
 
 	version = "0.1.0",
 
 	author="Sanketh Mopuru",
 	author_email="sanketh.mopuru@gmail.com",
 
-	packages=["argparse", "json", "sys", "urllib2", "cookielib", "random", "time","math","threading", "urllib", "os"],
+	url='https://github.com/sanketh95/coursera-offline',
+	license='GPL',
 
-	description = "Download your favorite course videos from Coursera in one go"
-	long_description = open("README.md").read()
+	description = "Download Coursera videos for offline viewing",
+	long_description = open("README.txt").read(),
 
 	install_requires=[
 		"pyquery"
