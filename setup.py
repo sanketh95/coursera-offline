d199542ff8260c67d671808b487cc5aeba4df206
Sanketh Mopuru <sanketh.mopuru@gmail.com>
2014-10-05 18:25:10 +0530

    Made changes to copy script to bin

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup
 
 setup(
-	name = "Coursera Offline",
+	name = "coursera_offline",
 
-	version = "0.1.0",
+	version = "0.1.1",
 
 	author="Sanketh Mopuru",
 	author_email="sanketh.mopuru@gmail.com",
 
 	url='https://github.com/sanketh95/coursera-offline',
-	license='GPL',
+	license='GPLv3',
 
-	description = "Download Coursera videos for offline viewing",
-	long_description = open("README.txt").read(),
+	description = 'Download Coursera videos for offline viewing',
 
 	install_requires=[
 		"pyquery"
 	],
 
+
+	scripts=['coursera_offline']
 	)
