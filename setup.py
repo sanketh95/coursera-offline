1c0173e04a20ecafe8bb92049cc253ec42cb7648
Sanketh Mopuru <sanketh.mopuru@gmail.com>
2014-10-12 15:15:14 +0530

    Setup config updated

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@ from setuptools import setup
 setup(
 	name = "coursera_offline",
 
-	version = "0.2.3",
+	version = "0.2.4",
 
 	author="Sanketh Mopuru",
 	author_email="sanketh.mopuru@gmail.com",
 
 	url='https://github.com/sanketh95/coursera-offline',
 	license='GPLv3',
+
+	install_requires = ['docutils>=0.3'],
+
+	package_data = {
+	        # If any package contains *.txt or *.rst files, include them:
+	        '': ['*.rst', '*.md'],
+	    },
 
 	description = 'Download Coursera videos for offline viewing',
 
@@ -18,6 +25,7 @@ setup(
 		"python-crontab"
 	],
 
+	keywords = "coursera offline download lecture lectures video videos"
 
 	scripts=['coursera_offline']
 	)
